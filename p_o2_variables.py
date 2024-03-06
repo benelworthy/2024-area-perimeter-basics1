0b93b850541c345a18dfaa9dcddf8447a4ace5ee
ben elworthy <161521475+benelworthy@users.noreply.github.com>
2024-03-07 12:58:52 +1300

    hi

---
+++ p_o2_variables.py
@@ -0,0 +1,13 @@
+# code to lirn about variables...
+
+# variables cn be a string (note the speach marks)
+greeting = "hello world"
+
+# variables can be a number
+fav_num = 7
+num_string =  "5"
+
+# constants are upper case
+PI = 3.14
+
+a_01_my_num = 7
